779e4d45d0d62a39f9f89e323837e7e8cf67099f
Jesse Hopkins <hopkins.jesse@gmail.com>
2018-04-25 11:42:22 -0500

    Tweaked plotBeamSize to not do absolute values, and to use numpy gradient for the derivative. Added argparse for the input.

--- beam_size/plotBeamSize.py
+++ beam_size/plotBeamSize.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
-import sys
+import argparse
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import scipy.interpolate
 
-fname = sys.argv[1]
+parser = argparse.ArgumentParser(description='Replot the beamsize from a saved scan.')
+parser.add_argument('name', help='The filename to plot.')
+
+args = parser.parse_args()
+
+fname = args.name
 
 f = h5py.File(fname, 'r')
 remark = np.array(f['remark'])
-print remark
+if remark:
+    print remark
 fwhm = np.array(f['FWHM'])
-print 'FWHM = %.03f mm' %fwhm
+print 'Saved FWHM = %.02f um' %(fwhm*1000)
 
 raw = np.array(f['raw-scan'])
 motor_position = raw[:,0] #first column
@@ -19,16 +25,18 @@ incident_I = raw[:,1] # 2nd column
 knifeedge_I = raw[:,2] # 3rd column
 
 normalized_I = np.divide(knifeedge_I, incident_I)
-diff_I = np.diff(normalized_I) # take derivative
+diff_I = np.gradient(normalized_I, motor_position) # take derivative
 diff_I[np.isnan(diff_I)] = 0 # convert all NaNs (divZero) to 0
 
-x = motor_position[:-1] #number of data points reduced by 1 after taking derivative
-y = np.fabs(diff_I) - np.max(np.fabs(diff_I))/2
+if normalized_I[:5].mean() > normalized_I[-5:].mean():
+    diff_I = diff_I*-1
 
-if x[0]>x[1]:
-    spline = scipy.interpolate.UnivariateSpline(x[::-1], y[::-1], s=0)
+y = diff_I - np.max(diff_I)/2
+
+if motor_position[0]>motor_position[1]:
+    spline = scipy.interpolate.UnivariateSpline(motor_position[::-1], y[::-1], s=0)
 else:
-    spline = scipy.interpolate.UnivariateSpline(x, y, s=0)
+    spline = scipy.interpolate.UnivariateSpline(motor_position, y, s=0)
 try:
     roots = spline.roots()
     if roots.size == 2:
@@ -47,7 +55,7 @@ except Exception as e:
     r2 = 0
 
 
-fwhm = np.fabs(r2-r1)
+fwhm = abs(r2-r1)
 
 
 plt.figure(1)
@@ -56,14 +64,14 @@ plt.plot(motor_position, normalized_I, 'bo')
 plt.title('Knife-edge step scan')
 
 plt.subplot(212)
-plt.plot(x, abs(diff_I))
+plt.plot(motor_position, diff_I)
 
 if fwhm>0:
     if r1<r2:
         plt.axvspan(r1, r2, facecolor='g', alpha=0.5)
     else:
         plt.axvspan(r2, r1, facecolor='g', alpha=0.5)
-    plt.hlines(np.max(abs(diff_I))/2, np.min(x), np.max(x))
+    plt.hlines(np.max(diff_I)/2, np.min(motor_position), np.max(motor_position))
     plt.title('FWHM = %.02f $\mu$m' %(fwhm*1000))
 else:
     plt.title('FWHM not found')
